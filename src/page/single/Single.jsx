f28749b871478d135a0878bcb7ec05b8b6ff5448
FelipeMDantas <felipemdantas@terra.com.br>
2022-05-02 20:04:03 -0300

    Transactions and users tables construction and stylization finished.

--- src/page/single/Single.jsx
+++ src/page/single/Single.jsx
@@ -1,7 +1,31 @@
 import "./single.scss";
+import Sidebar from "../../components/sidebar/Sidebar";
+import Navbar from "../../components/navbar/Navbar";
 
 const Single = () => {
-  return <div>Single</div>;
+  return (
+    <div className="single">
+      <Sidebar />
+      <div className="singleContainer">
+        <Navbar />
+        <div className="top">
+          <div className="left">
+            <h1 className="title">Information</h1>
+            <div className="item">
+              <img
+                src="https://images.pexels.com/photos/1382731/pexels-photo-1382731.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+                alt=""
+                className="itemImg"
+              />
+              details
+            </div>
+          </div>
+          <div className="right"></div>
+        </div>
+      </div>
+      <div className="bottom"></div>
+    </div>
+  );
 };
 
 export default Single;
