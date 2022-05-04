2f93fcbb33d4c88054dd9026763a6890d5c14a31
FelipeMDantas <felipemdantas@terra.com.br>
2022-05-03 23:33:14 -0300

    Individualized screen further developed.

--- src/page/single/Single.jsx
+++ src/page/single/Single.jsx
@@ -1,6 +1,8 @@
 import "./single.scss";
 import Sidebar from "../../components/sidebar/Sidebar";
 import Navbar from "../../components/navbar/Navbar";
+import Chart from "../../components/chart/Chart";
+import List from "../../components/table/Table";
 
 const Single = () => {
   return (
@@ -10,6 +12,7 @@ const Single = () => {
         <Navbar />
         <div className="top">
           <div className="left">
+            <div className="editButton">Edit</div>
             <h1 className="title">Information</h1>
             <div className="item">
               <img
@@ -17,13 +20,38 @@ const Single = () => {
                 alt=""
                 className="itemImg"
               />
-              details
+              <div className="details">
+                <h1 className="itemTitle">Jane Doe</h1>
+                <div className="detailItem">
+                  <span className="itemKey">Email:</span>
+                  <span className="itemVal">janedoe@gmail.com</span>
+                </div>
+                <div className="detailItem">
+                  <span className="itemKey">Phone:</span>
+                  <span className="itemVal">+1 2345 67 89</span>
+                </div>
+                <div className="detailItem">
+                  <span className="itemKey">Address:</span>
+                  <span className="itemVal">
+                    Tokyo, Setagaya City, Sangenjaya
+                  </span>
+                </div>
+                <div className="detailItem">
+                  <span className="itemKey">Country:</span>
+                  <span className="itemVal">Japan</span>
+                </div>
+              </div>
             </div>
           </div>
-          <div className="right"></div>
+          <div className="right">
+            <Chart aspect={3 / 1} title="User Spending (Last 5 Months)" />
+          </div>
+        </div>
+        <div className="bottom">
+          <h1 className="title">Latest Transactions</h1>
+          <List />
         </div>
       </div>
-      <div className="bottom"></div>
     </div>
   );
 };
